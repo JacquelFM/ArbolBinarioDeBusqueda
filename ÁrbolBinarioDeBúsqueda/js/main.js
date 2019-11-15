ad007780fa06b0fd48abdc4bee134ad60d58b33c
JacquelineLK119 <jflores43@ucol.mx>
2019-11-15 12:28:36 -0600

    Estructura - Plus

--- ÁrbolBinarioDeBúsqueda/js/main.js
+++ ÁrbolBinarioDeBúsqueda/js/main.js
@@ -6,7 +6,21 @@ class Main {
         let inventario = new Inventario();
 
         document.querySelector("#btnAgregar").addEventListener("click", () => {
-            inventario.agregarProducto();
+            let intCodigo = document.querySelector("#numCodigo").value;
+            let intNombre = document.querySelector("#nombreArticulo").value;
+            let intPrecio = Number(document.querySelector("#precioArticulo").value);
+            let intCantidad = Number(document.querySelector("#cantidad").value);
+            let intDescripcion = document.querySelector("#descripcionArticulo").value;
+
+            inventario.agregarProducto(
+                new Producto(
+                    intCodigo,
+                    intNombre,
+                    intPrecio,
+                    intCantidad,
+                    intDescripcion
+                ));
+
         });
 
         document.querySelector("#btnBuscar").addEventListener("click", () => {
